6b23dc32b963eb7b98304f8af5d7daa21c62bf8b
Daniel Rodriguez <daniel@danielfrg.com>
2021-04-05 17:55:48 -0500

    Initial Commit

---
+++ Shared/CantusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CantusApp.swift
+//  Shared
+//
+//  Created by Daniel Rodriguez on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CantusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
